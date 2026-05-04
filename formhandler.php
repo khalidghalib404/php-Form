39af38fee9ef2f38853f79d7b3572ff63c93a0ed
Khalid ghlaib <khalidghalib426@gmail.com>
2026-05-04 22:41:54 +0430

    post form

--- formhandler.php
+++ formhandler.php
@@ -1,16 +1,54 @@
 <?php 
-echo "{$_GET['fullname']} <br>";
-echo "{$_GET['email']} <br>";
-echo "{$_GET['password']}  <br>";
-echo "{$_GET['age']} <br> ";
-echo "{$_GET['gender']} <br>";
-echo "{$_GET['department']}<br>";
-echo "{$_GET['course']} <br>" ;
-echo "{$_GET['country']} <br>";
-echo "{$_GET['comment']}<br>";
-echo "{$_GET['hight']}<br>";
-echo "{$_GET['weight ']}<br>";
-echo "{$_GET['edt']}<br>";
-echo "{$_GET['date']}<br>";
+// Basic form handler with validation and sanitization
 
+function sanitize($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+echo "<h2>Submitted Data:</h2>";
+
+// Handle text inputs
+if (isset($_POST['fullname'])) {
+    echo "Full Name: " . sanitize($_POST['fullname']) . "<br>";
+}
+
+if (isset($_POST['email'])) {
+    echo "Email: " . sanitize($_POST['email']) . "<br>";
+}
+
+if (isset($_POST['password'])) {
+    // Don't display password for security
+    echo "Password: [hidden]<br>";
+}
+
+if (isset($_POST['age'])) {
+    echo "Age: " . sanitize($_POST['age']) . "<br>";
+}
+
+if (isset($_POST['gender'])) {
+    echo "Gender: " . sanitize($_POST['gender']) . "<br>";
+}
+
+if (isset($_POST['department'])) {
+    echo "Department: " . sanitize($_POST['department']) . "<br>";
+}
+
+// Handle checkboxes (can have multiple values)
+if (isset($_POST['course']) && is_array($_POST['course'])) {
+    $courses = implode(', ', array_map('sanitize', $_POST['course']));
+    echo "Courses: " . $courses . "<br>";
+} elseif (isset($_POST['course'])) {
+    echo "Course: " . sanitize($_POST['course']) . "<br>";
+}
+
+if (isset($_POST['country'])) {
+    echo "Country: " . sanitize($_POST['country']) . "<br>";
+}
+
+if (isset($_POST['comment'])) {
+    echo "Comment: " . nl2br(sanitize($_POST['comment'])) . "<br>";
+}
 ?>
